8e31574ea9d0ed0bb7a66f9085f06b389db14d70
Decronus <zick3333@mail.ru>
2023-03-03 18:51:33 +0100

    getting json

--- firebase.ts
+++ firebase.ts
@@ -1,31 +1,45 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, child, get } from "firebase/database";
+import { getDatabase, ref, onValue, set } from "firebase/database";
 const firebaseConfig = {
     databaseURL:
         "https://rocket-test-2933b-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
-const app = initializeApp(firebaseConfig);
-
-const database = getDatabase(app);
+const app = initializeApp(firebaseConfig)
+export const database = getDatabase(app);
 
 interface Tokens {
     access_token: string;
     refresh_token: string;
 }
 
-export async function getTokens(): Promise<any> {
-    const dbRef = ref(database);
-    get(child(dbRef, `/`))
-        .then((snapshot) => {
-            if (snapshot.exists()) {
-                // console.log(snapshot.val())
-                return snapshot.val();
-            } else {
-                return "No data available";
-            }
-        })
-        .catch((error) => {
-            return error;
-        });
+export let tokens: Tokens = {
+    access_token: null,
+    refresh_token: null
+};
+
+const tokensRef = ref(database, '/' );
+onValue(tokensRef, (snapshot) => {
+    tokens = snapshot.val();
+})
+
+async function updateTokens(access_token, refresh_token) {
+    set(ref(database, '/'), {
+        access_token: tokensResponseJson.access_token,
+        refresh_token: tokensResponseJson.refresh_token
+    })
+    .then(() => {
+        fetch(process.env.AMO_API_URL + "api/v4/leads", {
+            headers: {
+                Authorization: `Bearer ${tokensResponseJson.access_token}`
+                }
+            })
+
+        if (leads.status !== 401) {
+            return leads.ok ? leads.json() : `Response error: ${leads.status}`
+        }
+    })
+    .catch((error) => {
+    // The write failed...
+    });
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
-import { getTokens } from '../firebase.js'
+import { tokens, database } from "firebase";
+import { ref, set } from "firebase/database";
 
 interface Tokens {
     access_token: string;
@@ -9,18 +10,56 @@ interface Tokens {
 @Injectable()
 export class AppService {
     async getLeads() {
-        // const tokens = await getTokens();
-
-        // setTimeout(() => {
-        //     console.log('tokens', tokens);
-        // }, 5000);
-
-
         const leads = await fetch(process.env.AMO_API_URL + "api/v4/leads", {
             headers: {
                 Authorization: `Bearer ${tokens.access_token}`
+                }
+            })
+
+        if (leads.status === 401) {
+            return leads.ok ? leads.json() : `Response error: ${leads.status}`
+        } else {
+            const body = {
+                client_id: "b9859249-ccaf-4197-8eb6-d26d42140026",
+                client_secret: "Ujq1NhZSQJFh9ZIanee1ai1Mvd3ewZ4uMgon6r0yHRONEqkraDqXJQCqCgzMRE5s",
+                grant_type: "refresh_token",
+                refresh_token: `${tokens.refresh_token}`,
+                redirect_uri: "https://localhost.com"
+
             }
-        })
-        return leads.ok ? leads.json() : `Response error: ${leads.status}`;
+            const updateTokensResponse = await fetch(process.env.AMO_API_URL + "oauth2/access_token", {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${tokens.access_token}`,
+                    "Content-Type": 'application/json'
+                    },
+                body: JSON.stringify(body)
+            })
+
+            const tokensResponseJson: Tokens = await updateTokensResponse.json()
+            console.log('body', updateTokensResponse);
+
+            if (updateTokensResponse.ok) {
+                set(ref(database, '/'), {
+                    access_token: tokensResponseJson.access_token,
+                    refresh_token: tokensResponseJson.refresh_token
+                })
+                .then(() => {
+                    fetch(process.env.AMO_API_URL + "api/v4/leads", {
+                        headers: {
+                            Authorization: `Bearer ${tokensResponseJson.access_token}`
+                            }
+                        })
+
+                    if (leads.status !== 401) {
+                        return leads.ok ? leads.json() : `Response error: ${leads.status}`
+                    }
+                })
+                .catch((error) => {
+                // The write failed...
+                });
+            }
+        }
+
     }
 }
