4d3f3ce81d072f33a5d296576caaf6e98cdc291f
Decronus <zick3333@mail.ru>
2023-03-04 21:55:53 +0100

    queries ref

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -6,90 +6,62 @@ import Queries from './queries.service'
 @Injectable()
 export class AppService {
     async getMainData() {
-        // let leads: object = {}
+        let mainData: any[] = []
 
-        const leads = await Queries.getLeads()
+        const leads = await Queries.getLeads();
 
-        // console.log("leads.status", leads.status);
+        if (leads.status !== 401 && leads.statusText === "OK") {
 
-        return leads.data;
+            leads.data._embedded.leads.forEach((lead) => {
+                let leadObj = {
+                    name: null,
+                    price: null,
+                    responsible_user_id: null,
+                    status_name: null,
+                    status_color: null
+                }
 
-        // const leads: Response = await fetch(process.env.AMO_API_URL + "api/v4/leads", {
-        //     headers: {
-        //         Authorization: `Bearer ${tokens.access_token}`
-        //         }
-        //     });
-        // const leadsJson = await leads.json()
+                leadObj.name = lead.name;
+                leadObj.price = lead.price;
+                leadObj.responsible_user_id = lead.responsible_user_id;
 
-        // if (leads.status !== 401 && leads.ok) {
-        //     // return leads.ok ? leads.json() : `Response error: ${leads.status}`;
-        //     leadsJson._embedded.leads.forEach((lead) => {
-        //         let leadObj = {
-        //             name: null,
-        //             price: null,
-        //             responsible_user_id: null,
-        //             status_name: null,
-        //             status_color: null
-        //         }
+                mainData.push(leadObj);
 
-        //         leadObj.name = lead.name;
-        //         leadObj.price = lead.price;
-        //         leadObj.responsible_user_id = lead.responsible_user_id;
+                console.log(mainData)
+            })
 
-        //         mainData.push(leadObj);
+            const pipeline = await Queries.getPipelineByID("6531006")
 
-        //         console.log(mainData)
-        //     })
+            pipeline.data._embedded.statuses.forEach(status => {
+                const index: number = pipeline.data._embedded.statuses.indexOf(status)
+                console.log('pipelinedata', status)
+                console.log(typeof index)
+                console.log("mainData", mainData)
+                console.log("mainData[index]", mainData[index])
+                console.log("mainData[0]", mainData[0])
+                mainData[0].status_name = status.name;
+                mainData[0].status_color = status.color;
+            })
 
-        //     const pipeline: Response = await fetch(process.env.AMO_API_URL + "api/v4/leads/pipelines/6531006", {
-        //         headers: {
-        //             Authorization: `Bearer ${tokens.access_token}`
-        //             }
-        //         });
-        //     const pipelineJson = await pipeline.json();
-        //     console.log("pipelineJson", pipelineJson)
+            return mainData;
+        } else {
 
-        //     pipelineJson._embedded.statuses.forEach(status => {
-        //         const index: number = pipelineJson._embedded.statuses.indexOf(status)
-        //         console.log(index)
-        //         console.log("mainData[index]", mainData[index])
-        //         mainData[0].status_name = status.name;
-        //         mainData[0].status_color = status.color;
-        //     })
+            const body: UpdateTokensBody = {
+                client_id: process.env.CLIENT_ID,
+                client_secret: process.env.CLIENT_SECRET,
+                grant_type: "refresh_token",
+                refresh_token: `${tokens.refresh_token}`,
+                redirect_uri: "https://localhost.com"
+            };
+            const updateTokensResponse = await Queries.postUpdateTokenResponse(body);
 
-        //     return mainData;
-        // } else {
+            if (updateTokensResponse.statusText === "OK") {
+                updateTokens(updateTokensResponse.data.access_token, updateTokensResponse.data.refresh_token);
 
-        //     const body: UpdateTokensBody = {
-        //         client_id: "b9859249-ccaf-4197-8eb6-d26d42140026",
-        //         client_secret: "Ujq1NhZSQJFh9ZIanee1ai1Mvd3ewZ4uMgon6r0yHRONEqkraDqXJQCqCgzMRE5s",
-        //         grant_type: "refresh_token",
-        //         refresh_token: `${tokens.refresh_token}`,
-        //         redirect_uri: "https://localhost.com"
-        //     };
-        //     const updateTokensResponse: Response = await fetch(process.env.AMO_API_URL + "oauth2/access_token", {
-        //         method: "POST",
-        //         headers: {
-        //             Authorization: `Bearer ${tokens.access_token}`,
-        //             "Content-Type": 'application/json'
-        //             },
-        //         body: JSON.stringify(body)
-        //     });
+                const leads = await Queries.getLeads();
 
-        //     if (updateTokensResponse.ok) {
-        //         const tokensResponseJson: Tokens = await updateTokensResponse.json();
-        //         updateTokens(tokensResponseJson.access_token, tokensResponseJson.refresh_token);
-
-        //         const leads: Response = await fetch(process.env.AMO_API_URL + "api/v4/leads", {
-        //                 headers: {
-        //                     Authorization: `Bearer ${tokens.access_token}`
-        //                     }
-        //                 });
-
-        //         return leads.ok ? leads.json() : `Response error: ${leads.status}`;
-
-        //     }
-        // }
-
+                return leads.data;
+            }
+        }
     }
 }
--- server/src/queries.service.ts
+++ server/src/queries.service.ts
@@ -3,61 +3,26 @@ import { auth } from "./axiosInstance";
 
 class Queries {
     getLeads() {
-        return axiosInstance.get("api/v4/leads", {
-            headers: auth(),
-        });
+        return axiosInstance.get(
+            "api/v4/leads",
+            { headers: auth() }
+        );
     }
 
-    // getAllCourseLessons(id) {
-    //     return axiosIns.get(`courses/${id}/lessons/`, {
-    //         headers: auth(),
-    //     });
-    // }
+    getPipelineByID(id: string) {
+        return axiosInstance.get(
+            `api/v4/leads/pipelines/${id}`,
+            { headers: auth() }
+        );
+    }
 
-    // getLessonById(lesson_id) {
-    //     return axiosIns.get(`courses/lesson/${lesson_id}`, {
-    //         headers: auth(),
-    //     });
-    // }
-
-    // getCourseById(id) {
-    //     return axiosIns.get(`courses/${id}`, {
-    //         headers: auth(),
-    //     });
-    // }
-
-    // postAddToWishlist(body) {
-    //     return axiosIns.post(
-    //         "courses/wishlist/add/",
-    //         body,
-    //         {headers: auth()}
-    //     );
-    // }
-
-    // postDeleteFromWishlist(body) {
-    //     return axiosIns.post(
-    //         "courses/wishlist/delete/",
-    //         body,
-    //         {headers: auth()}
-    //     );
-    // }
-
-    // postAddLike(body) {
-    //     return axiosIns.post(
-    //         "courses/like/add/",
-    //         body,
-    //         {headers: auth()}
-    //     );
-    // }
-
-    // postDeleteLike(body) {
-    //     return axiosIns.post(
-    //         "courses/like/delete/",
-    //         body,
-    //         {headers: auth()}
-    //     );
-    // }
-
+    postUpdateTokenResponse(body) {
+        return axiosInstance.post(
+            `oauth2/access_token`,
+            body,
+            { headers: auth() }
+        );
+    }
 }
 
 export default new Queries();
